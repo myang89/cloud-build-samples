f8a08485fe40e008e0c1569acffdce867fb34b83
myang89 <myang89@lgcns.com>
2021-10-26 15:16:04 +0000

    update blah blah

--- golang-sample/main.go
+++ golang-sample/main.go
@@ -43,7 +43,7 @@ func main() {
 func handler(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	if name == "" {
-		name = "World"
+		name = "MYANG89 World"
 	}
 	fmt.Fprintf(w, "Hello %s!\n", name)
 }
